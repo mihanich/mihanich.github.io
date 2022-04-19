dbf61452e095484e3a065fecd8c7d5d4560874c6
Mihanich Yavin <mixanich@gmail.com>
2022-04-19 17:50:54 +0300

    new portion of work

--- js/common.js
+++ js/common.js
@@ -1,6 +1,11 @@
 document.addEventListener('DOMContentLoaded', function() {
 
-
+    const showSlider = new Swiper('showcase-carousel', {
+        loop:true,
+        slidesPerView: 3,
+        speed: 1800,
+        centeredSlides: true,
+    })
 
     document.querySelector('video').playbackRate = 0.8
 
