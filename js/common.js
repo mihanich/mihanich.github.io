524d0046e7ab0b2ecc2de272e77d93cfd2681794
Mihanich Yavin <mixanich@gmail.com>
2022-04-19 18:54:12 +0300

    fixes and fin

--- js/common.js
+++ js/common.js
@@ -5,6 +5,10 @@ document.addEventListener('DOMContentLoaded', function() {
         slidesPerView: 3,
         speed: 1800,
         centeredSlides: true,
+        navigation: {
+            nextEl: '.showcase-navigation__next',
+            nextEl: '.showcase-navigation__prev'
+        }
     })
 
     document.querySelector('video').playbackRate = 0.8
